fde23e29ab7f6523119b3a984eec0d12f971e479
AIMAN *͘⁴̅⁰͍⁴̵〆* <82744566+ai-man-123@users.noreply.github.com>
2022-11-05 21:41:03 +0800

    Update info-rules.js

--- plugins/info-rules.js
+++ plugins/info-rules.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn }) => {
 bear = "Source Code"
-let esce = `Cyno Bot Adalah Project Script Bot Md.. Ownerku @62887433094409 , Cyno & Ownerku Gak Suka Spam, Telpon , Org Negri , Invite bot tanpa izin block + ban 🚫`
+let esce = `Cyno Bot Adalah Project Script Bot Md.. Ownerku @60102810046 , Cyno & Ownerku Gak Suka Spam, Telpon , Org Negri , Invite bot tanpa izin block + ban 🚫`
 conn.sendButtonImg(m.chat, fla + 'Rules', esce, wm2, 'Menu', '.menu', m) 
 }
 handler.help = ['rules']
